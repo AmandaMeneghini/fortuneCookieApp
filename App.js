59a9862a0e983ef3c9592079e24817f01c28b2b5
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-13 17:57:48 -0300

    style: update component styles for improved layout and appearance
    This commit refactors the component's styles to enhance its layout and visual presentation.
    
    Changes include:
    - Centering content within the container.
    - Styling the text phrase with a specific font size, color, margin, style, and alignment.
    - Styling the button with defined dimensions, border, and border radius.
    - Styling the button area for centered content.
    - Styling the button text with a specific font size, weight, and color.

--- App.js
+++ App.js
@@ -16,12 +16,12 @@ class App extends Component {
         style={styles.img}
         />
 
-        <Text style={styles.textPhase}>Alguma frase</Text>
+        <Text style={styles.textPhase}>"Alguma Frase Aqui"</Text>
 
         <TouchableOpacity style={styles.button}>
-          <View style={styles.btnAre}>
+          <View style={styles.btnArea}>
             <Text style={styles.btnText}>
-              Abrir biscoito
+              Quebrar biscoito
             </Text>
           </View>
         </TouchableOpacity>
@@ -34,11 +34,38 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   img: {
     width: 250,
     height: 250
   },
+  textPhase:{
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 20,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  button: {
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25 
+  },
+  btnArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  }
 });
 
 export default App;

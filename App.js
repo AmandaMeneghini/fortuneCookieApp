12329ae1084114475575a188b028a08680591148
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-13 17:13:39 -0300

    refactor: rename App file extension from .jsx to .js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; // importa o React
+import { View, Text } from 'react-native'; //importa o componente "View" do React Native
+
+class App extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Olá</Text>
+      </View>
+    );
+  }
+}
+
+export default App;

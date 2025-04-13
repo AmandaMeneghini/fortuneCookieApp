6f53ab1c8145d79a2766dc2d678594a5d079ba49
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-13 17:21:22 -0300

    feat(style): imports StyleSheet creates style for the container and implements it in the View component

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'; 
-import { View, Text, StyleSheet } from 'react-native'; 
+import { 
+  View,
+  Text,
+  StyleSheet 
+  } from 'react-native'; 
 
 class App extends Component {
   render() {
     return(
-      <View>
+      <View style={styles.container}>
         
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
